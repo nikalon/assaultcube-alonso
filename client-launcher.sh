5bd3a7de6b1567f8e41ca85515d9778a23a7fbd4
Miguel Gutiérrez <nikalon@users.noreply.github.com>
2017-01-26 20:12:18 +0000

    Fix semicolon (OMG) and GL env variables

--- client-launcher.sh
+++ client-launcher.sh
@@ -2,17 +2,18 @@
 
 case ${SNAP_ARCH} in
 amd64)
-  ARCH="x86_64-linux-gnu"
+  ARCH="x86_64-linux-gnu";;
 i386)
-  ARCH="i386-linux-gnu"
+  ARCH="i386-linux-gnu";;
 armhf)
-  ARCH="arm-linux-gnueabihf"
+  ARCH="arm-linux-gnueabihf";;
 arm64)
-  ARCH="aarch64-linux-gnu"
+  ARCH="aarch64-linux-gnu";;
 esac
 
-export LD_LIBRARY_PATH="${SNAP}/usr/lib/$ARCH/dri:${LD_LIBRARY_PATH}"
+export LIBGL_DRIVERS_PATH=${SNAP}/usr/lib/$ARCH/dri:${LD_LIBRARY_PATH}
 CUBE_OPTIONS="--home=${SNAP_USER_DATA} --init"
 
-exec "$SNAP/bin/bin_unix/native_client" "$@"
+cd ${SNAP}/bin
+exec "bin_unix/native_client" "$@"
 
